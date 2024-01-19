80ccf45c044eb2ae678ef2f5b574d8eb80277159
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-01-19 20:50:45 +0530

    added customers page data grid

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./scenes/layout/Layout";
 import Dashboard from "./scenes/dashboard/Dashboard";
 import Products from "./scenes/products/Products";
+import Customers from "./scenes/customers/Customers";
 
 function App() {
   const mode = useSelector(state => state.global.mode);
@@ -21,6 +22,7 @@ function App() {
               <Route path='/' element={<Navigate to='/dashboard' replace />} />
               <Route path='/dashboard' element={<Dashboard />} />
               <Route path='/products' element={<Products />} />
+              <Route path='/customers' element={<Customers />} />
             </Route>
           </Routes>
         </ThemeProvider>
